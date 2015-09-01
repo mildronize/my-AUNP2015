de3650d9a48f88c762f029da2b9c7f3be895de14
Thada Wangthammang <mildronize@gmail.com>
2015-09-01 12:08:53 +0700

    edit ex1.c to simple input and output

--- temperature/ex1.c
+++ temperature/ex1.c
@@ -12,9 +12,18 @@ struct info{
 };
 
 int main(int argc, char **agrv){
-  int i;
-  for( i=0;i < 2;i++){
-    printf("%s %3d %2.1f %2.2f\n", n[i].name, n[i].age, n[i].height, n[i].weight );
+  int i=0;
+  char name[30];
+  int age;
+  float height, weight;
+
+  while(1){
+    int count= scanf ("%s %d %f %f", name, &age, &height, &weight);
+    if (count !=5 )break;
+    printf(">> %s %3d %2.1f %2.2f\n", name, age, height, weight );
   }
+  /* for( i=0;i < 2;i++){ */
+    /* printf("%s %3d %2.1f %2.2f\n", n[i].name, n[i].age, n[i].height, n[i].weight ); */
+  /* } */
   return 0;
 }
